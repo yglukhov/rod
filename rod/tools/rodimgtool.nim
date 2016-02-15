88b09a006f4d8bc7aeba4ce8bf156e4f032b1874
Yuriy Glukhov <yutiy.glukhov@gmail.com>
2016-02-15 19:37:51 +0200

    Fix for windows

--- rod/tools/rodimgtool.nim
+++ rod/tools/rodimgtool.nim
@@ -1,12 +1,17 @@
 import cligen
 import imgtool
 import times
+import os
 
 proc rodimgtool(originalResPath: string = ".", resPath: string = ".", outPrefix: string = ".", nocompress: bool = false, compositions: seq[string]): int =
     var tool = newImgTool()
     tool.originalResPath = originalResPath
     tool.resPath = resPath
-    tool.compositionPaths = compositions
+
+    tool.compositionPaths = @[]
+    for c in compositions:
+        for i in walkFiles(c):
+            tool.compositionPaths.add(i)
     tool.outPrefix = outPrefix
     tool.compressOutput = not nocompress
     let startTime = epochTime()
