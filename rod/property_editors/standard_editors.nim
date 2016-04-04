a3132672c49692838ee27b90dd55f057aa88bf82
bogdan-voevoda <bs_voevoda@ukr.net>
2016-04-04 22:21:09 +0300

    add color picker to editor view

--- rod/property_editors/standard_editors.nim
+++ rod/property_editors/standard_editors.nim
@@ -5,6 +5,7 @@ import nimx.text_field
 import nimx.matrixes
 import nimx.image
 import nimx.button
+import nimx.color_picker
 
 import rod.property_editors.propedit_registry
 import rod.numeric_text_field
@@ -15,6 +16,8 @@ when defined(js):
     from dom import alert
 elif not defined(android) and not defined(ios):
     import native_dialogs
+
+var gColorPicker*: ColorPickerView
 
 template toStr(v: SomeReal, precision: uint): string = formatFloat(v, ffDecimal, precision)
 template toStr(v: SomeInteger): string = $v
@@ -98,6 +101,36 @@ proc newColorPropertyView(setter: proc(s: Color), getter: proc(): Color): View =
                 )
             setter(c)
             colorView.backgroundColor = c
+
+            if not gColorPicker.isNil:
+                gColorPicker.removeFromSuperview()
+                gColorPicker = nil
+
+            if gColorPicker.isNil:
+                gColorPicker = newColorPickerView(newRect(0, 0, 300, 200))
+                gColorPicker.setFrameOrigin(newPoint(pv.frame.x+140+300, 0))
+
+                let pickerCloseButton = Button.new(newRect(0, 0, 25, 25))
+                pickerCloseButton.title = "x"
+                pickerCloseButton.onAction do():
+                    gColorPicker.removeFromSuperview()
+                    gColorPicker = nil
+                gColorPicker.addSubview(pickerCloseButton)
+                pv.window.addSubview(gColorPicker)
+
+                var pickerCurrColor = rgbToHSV(c.r, c.g, c.b)
+
+                gColorPicker.colorHasChanged(pickerCurrColor)
+                gColorPicker.onColorSelected = proc(pc: Color) =
+                    TextField(pv.subviews[1]).text = $pc.r
+                    TextField(pv.subviews[2]).text = $pc.g
+                    TextField(pv.subviews[3]).text = $pc.b
+                    TextField(pv.subviews[4]).text = $pc.a
+
+                    setter(pc)
+                    colorView.backgroundColor = pc
+
+                    gColorPicker.removeFromSuperview()
         except ValueError:
             discard
 
