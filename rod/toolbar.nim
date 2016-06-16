f44a9486741be68aa2285ba51357e105042bddbe
Yuriy Glukhov <yutiy.glukhov@gmail.com>
2016-06-17 02:20:51 +0300

    ui refactored a bit

--- rod/toolbar.nim
+++ rod/toolbar.nim
@@ -1,26 +1,18 @@
-import nimx.view, nimx.context
+import nimx.context
 import view_dragging_listener
+import linear_layout
 
-type Toolbar* = ref object of View
+type Toolbar* = ref object of LinearLayout
 
 method init*(v: Toolbar, r: Rect) =
-    procCall v.View.init(r)
+    procCall v.LinearLayout.init(r)
+    v.horizontal = true
+    v.leftMargin = 10
+    v.padding = 3
+    v.topMargin = 3
+    v.bottomMargin = 3
+    v.rightMargin = 3
     v.enableDraggingByBackground()
-
-const leftMargin = 10.Coord
-const xPadding = 3.Coord
-const verticalMargin = 3.Coord
-
-method resizeSubviews*(v: Toolbar, oldSize: Size) =
-    var x = leftMargin
-    let height = v.bounds.height - verticalMargin * 2
-    for s in v.subviews:
-        var fr = s.frame
-        fr.origin.x = x
-        fr.origin.y = verticalMargin
-        fr.size.height = height
-        x += fr.width + xPadding
-        s.setFrame(fr)
 
 method draw*(view: Toolbar, rect: Rect) =
     let c = currentContext()
@@ -28,13 +20,3 @@ method draw*(view: Toolbar, rect: Rect) =
     c.strokeColor = newGrayColor(0.6, 0.7)
     c.fillColor = newGrayColor(0.3, 0.7)
     c.drawRoundedRect(view.bounds, 5)
-
-proc updateWidth(v: Toolbar) =
-    var totalWidth = leftMargin
-    for s in v.subviews:
-        totalWidth += s.frame.width + xPadding
-    v.setFrameSize(newSize(totalWidth, v.frame.height))
-
-method addSubview*(v: Toolbar, s: View) =
-    procCall v.View.addSubview(s)
-    v.updateWidth()
