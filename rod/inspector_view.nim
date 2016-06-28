965602031b08a4949126d4885e2462d1e22601f9
Yuriy Glukhov <yutiy.glukhov@gmail.com>
2016-06-28 23:09:57 +0300

    Moved property_editors from rod to nimx

--- rod/inspector_view.nim
+++ rod/inspector_view.nim
@@ -7,6 +7,7 @@ import nimx.menu
 import nimx.scroll_view
 import nimx.panel_view
 import nimx.linear_layout
+import nimx.property_visitor
 
 import variant
 
@@ -16,9 +17,8 @@ import node
 import component
 import rod_types
 
-import property_visitor
-import property_editors.propedit_registry
-import property_editors.standard_editors
+import rod.property_editors.propedit_registry
+import rod.property_editors.standard_editors
 
 
 type InspectorView* = ref object of PanelView
--- rod/property_editors/propedit_registry.nim
+++ rod/property_editors/propedit_registry.nim
@@ -1,60 +1,10 @@
-import tables
 import nimx.view
-import nimx.text_field
-import nimx.font
-import nimx.property_visitor
+import nimx.property_editors.propedit_registry as npr
+
+export npr
 
 import rod.node
-import rod.meta_data
-
 import variant
 
-type PropertyEditorView* = ref object of View
-    onChange*: proc()
-    changeInspector*: proc()
-
-var propEditors = initTable[TypeId, proc(n: Node, v: Variant): PropertyEditorView]()
-
-proc registerPropertyEditor*[T](createView: proc(n: Node, setter: proc(s: T), getter: proc(): T): PropertyEditorView) =
-    propEditors[getTypeId(SetterAndGetter[T])] = proc(n: Node, v: Variant): PropertyEditorView =
-        let sng = v.get(SetterAndGetter[T])
-        result = createView(n, sng.setter, sng.getter)
-
-proc registerPropertyEditor*[T](createView: proc(setter: proc(s: T), getter: proc(): T): PropertyEditorView) =
-    propEditors[getTypeId(SetterAndGetter[T])] = proc(n: Node, v: Variant): PropertyEditorView =
-        let sng = v.get(SetterAndGetter[T])
-        result = createView(sng.setter, sng.getter)
-
-var gEditorFont: Font
-
-proc editorFont*(): Font =
-    if gEditorFont.isNil: gEditorFont = systemFontOfSize(14)
-    result = gEditorFont
-
-const editorRowHeight* = 16
-
 proc propertyEditorForProperty*(n: Node, title: string, v: Variant, onChangeCallback, changeInspectorCallback: proc()): View =
-    let creator = propEditors.getOrDefault(v.typeId)
-    result = View.new(newRect(0, 0, 328, editorRowHeight))
-    result.autoresizingMask = {afFlexibleWidth, afFlexibleMaxY}
-    let label = newLabel(newRect(0, 0, 100, editorRowHeight))
-    label.textColor = newGrayColor(0.9)
-    label.text = title & ":"
-    label.font = editorFont()
-    result.addSubview(label)
-    if creator.isNil:
-        label.text = title & " - Unknown property"
-    else:
-        let editor = creator(n, v)
-        editor.setFrameOrigin(newPoint(label.frame.width, 0))
-        var sz = newSize(result.bounds.width - label.frame.width, editor.frame.height)
-        editor.setFrameSize(sz)
-        editor.autoresizingMask = {afFlexibleWidth, afFlexibleMaxY}
-        result.addSubview(editor)
-
-        sz = result.frame.size
-        sz.height = editor.frame.height
-        result.setFrameSize(sz)
-
-        editor.onChange = onChangeCallback
-        editor.changeInspector = changeInspectorCallback
+    propertyEditorForProperty(newVariant(n), title, v, onChangeCallback, changeInspectorCallback)
