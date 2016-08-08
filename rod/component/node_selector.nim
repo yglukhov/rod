42e09bee4ba0071a3c2d204c0a01f58eb3766880
Андрей Гутыря <rrenderr@zeoalliance.com>
2016-08-08 15:11:58 +0300

    - fix crash on draging root

--- rod/component/node_selector.nim
+++ rod/component/node_selector.nim
@@ -197,7 +197,10 @@ proc proccesTransform*(ns: NodeSelector, position: Point) =
     curPosition = ns.node.sceneView.screenToWorldPoint(curScreenPoint) + offset
     curPosition = curPosition - ns.gizmo.worldPos
     ns.gizmo.position = ns.gizmo.worldPos + curPosition * ns.gizmoAxis
-    ns.node.position = ns.node.parent.worldToLocal(ns.gizmo.position)
+    if not ns.node.parent.isNil:
+        ns.node.position = ns.node.parent.worldToLocal(ns.gizmo.position)
+    else:
+        ns.node.position = ns.gizmo.position
 
 proc stopTransform*(ns: NodeSelector) =
     ns.gizmoAxis = newVector3(0.0, 0.0, 0.0)
