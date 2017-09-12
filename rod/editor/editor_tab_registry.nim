98ccedd6e0c277b54ad8ad2065837959727e9cf1
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2017-09-12 16:31:02 +0300

    Workaround nim+closure_compiler issue

--- rod/editor/editor_tab_registry.nim
+++ rod/editor/editor_tab_registry.nim
@@ -1,5 +1,5 @@
 import typetraits
-import nimx.types, nimx.view
+import nimx / [ types, view ]
 
 type
    EditViewEntry* = tuple
@@ -8,16 +8,19 @@ type
 
 var gRegisteredViews = newSeq[EditViewEntry]()
 
-template registerEditorTab*(tn: string, t: typedesc)=
-    registerClass(t)
+proc registerEditorTabAux(tn: string, t: typedesc) =
     var evr: EditViewEntry
     evr.name = tn
     let typename = typetraits.name(t)
-    evr.create = proc():View=
+    evr.create = proc(): View =
         result = newObjectOfClass(typename).View
         result.name = evr.name
 
     gRegisteredViews.add(evr)
+
+template registerEditorTab*(tn: string, t: typedesc) =
+    registerClass(t)
+    registerEditorTabAux(tn, t)
 
 iterator registeredEditorTabs*():EditViewEntry=
     for rt in gRegisteredViews:
