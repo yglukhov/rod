8699eae07e41a77a70790c72b59c0156ec8edb52
tormund <tormund@zeoalliance.com>
2017-11-03 17:39:53 +0200

    fix compilatioN

--- rod/editor/editor_default_tabs.nim
+++ rod/editor/editor_default_tabs.nim
@@ -3,5 +3,5 @@ import rod.editor.editor_tree_view
 import rod.editor.animation_edit_view
 import rod.editor.editor_scene_view
 
-when not (defined(js) or defined(emscripten)):
+when not (defined(js) or defined(emscripten) or defined(android) or defined(ios)):
     import rod.editor.editor_assets_view
