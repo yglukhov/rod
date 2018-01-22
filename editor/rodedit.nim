5df4ac3552167fb3cc3b2cf41bd197fdab886daa
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2018-01-22 16:59:23 +0200

    rodedit is a wrapper now

--- editor/rodedit.nim
+++ editor/rodedit.nim
@@ -1,1 +1,4 @@
-include rodedit_main
+# This is a wrapper to rodedit_main. It compiles rodedit_main with rod
+# plugins of the project that calls the wrapper.
+import ../rod/tools/tool_wrapper
+runWrapper("rodedit", "editor/rodedit_main.nim")
