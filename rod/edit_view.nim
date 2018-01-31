d3cf83384a195f4515d2d3c73133229feb243591
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2018-01-31 14:00:31 +0200

    Removed legacy stuff

--- rod/edit_view.nim
+++ rod/edit_view.nim
@@ -386,13 +386,7 @@ proc startEditingNodeInView*(n: Node, v: View, startFromGame: bool = true): Edit
 
 # default tabs hacky registering
 import nimx.assets.asset_loading
-import nimx.resource_cache
-import nimx.resource
 import nimx.assets.json_loading
-
-registerResourcePreloader(["json", "jcomp"]) do(name: string, callback: proc(j: JsonNode)):
-    loadJsonResourceAsync(name) do(j: JsonNode):
-        callback(j)
 
 registerAssetLoader(["json", "jcomp"]) do(url: string, callback: proc(j: JsonNode)):
     loadJsonFromURL(url, callback)
