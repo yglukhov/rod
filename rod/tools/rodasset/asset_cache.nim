9f1a5faa9f434f3d9ca80a9ec5212dbdb4bbfd6a
Volodymyr Melnychuk <valdema_r@hotmail.com>
2021-02-24 18:11:17 +0200

    bump assets versio (#370)
    Co-authored-by: tormund <tormund@oftengames.com>

--- rod/tools/rodasset/asset_cache.nim
+++ rod/tools/rodasset/asset_cache.nim
@@ -7,7 +7,7 @@ import settings
 
 # When asset packing algorithm changes, we should increase `hashVersion`
 # to invalidate old caches.
-const hashVersion = 12
+const hashVersion = 13
 
 const audioFileExtensions = [".wav", ".ogg", ".mp3"]
 
