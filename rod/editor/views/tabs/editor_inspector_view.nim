d4786d667b22d9f64118c45c722c6fe9cc77fe75
V. Melnychuk <valdema_r@hotmail.com>
2025-08-08 22:23:20 +0300

    wip inspector

--- rod/editor/views/tabs/editor_inspector_view.nim
+++ rod/editor/views/tabs/editor_inspector_view.nim
@@ -1,35 +1,147 @@
-import nimx / [ view, button, layout, types, text_field, scroll_view ]
-import ../../../../rod/[ node ]
-import ../../editor_types
+import std/[tables, hashes]
+import nimx / [ view, button, layout, types, text_field, scroll_view, property_visitor, popup_button ]
+import nimx/property_editors/[ standard_editors, propedit_registry ]
+import ../../../[ node, component, rod_types ]
+import ../../../property_editors/standard_editors
+import ../../editor_view_types
+import ../[editor_visitor_view, editor_add_component_view]
+import variant
 
 type EditorInspectorView* = ref object of EditorTabView
+  content: View
+  currentVisitors: seq[EditorPropertyVisitorView]
 
 method init*(v: EditorInspectorView) =
   procCall v.EditorTabView.init()
 
   v.makeLayout:
     backgroundColor: whiteColor()
-    - Label:
-      origin == super
-      width == super - 20
-      height == 20
-      text: "Auto update:"
-
-    - Checkbox as autoUpdate:
-      x == prev.x + prev.width
-      y == prev.y
-      height == 20
-      width == 20
-      onAction:
-        echo "checked ", autoUpdate.boolValue
-
     - ScrollView:
       x == super.x
-      y == 20
-      height == super - 20
+      y == super.y
+      height == super
       width == super
+      - View as content:
+        width == super
+
+  v.content = content
+
+proc inspectedNodeChanged(v: EditorInspectorView, n: Node) {.gcsafe.}
+
+proc createComponentsView(v: EditorInspectorView, n: Node) {.gcsafe.} =
+  var components = new(EditorAddComponentsView)
+  components.popupAtPoint(v.convertPointToWindow(v.frame.origin))
+  components.onAdd do(component: string):
+    echo "adding component ", component
+    discard n.addComponent(component)
+    v.inspectedNodeChanged(n)
+
+proc inspectedNodeChanged(v: EditorInspectorView, n: Node) {.gcsafe.}=
+  v.content.removeAllSubviews()
+  proc changeInspectorView() =
+    # i.inspectedNode = n
+    discard
+
+  var visitor : PropertyVisitor
+  visitor.requireName = true
+  visitor.requireSetter = true
+  visitor.requireGetter = true
+  # if i.editor.mode == emAnimation:
+  #     visitor.flags = { pfAnimatable }
+  # else:
+  visitor.flags = { pfEditable }
+
+  var visitorView = new(EditorPropertyVisitorView)
+  visitorView.setVisitorName("Node")
+  visitorView.makeLayout:
+    backgroundColor: newColor(0.123, 0.456, 0.123)
+    top == super.top
+    leading == super.leading
+    trailing == super.trailing
+
+  v.content.addSubview(visitorView)
+  v.currentVisitors.add(visitorView)
+
+  var lasPropView: View
+  visitor.commit = proc() =
+    var propView = propertyEditorForProperty(visitor.name, visitor.setterAndGetter, nil, changeInspectorView)
+    propView.makeLayout:
+      top == prev.bottom
+      leading == super.leading
+      trailing == super.trailing
+    visitorView.content.addSubview(propView)
+    lasPropView = propView
+
+  n.visitProperties(visitor)
+  if not lasPropView.isNil:
+    lasPropView.makeLayout:
+      bottom == super.bottom
+
+
+  var idx = 0
+
+  for com in n.components:
+    # closureScope:
+    #   discard
+    visitorView = new(EditorPropertyVisitorView)
+    # lastCompView = compView
+    visitorView.setVisitorName(com.className)
+    visitorView.makeLayout:
+      backgroundColor: uiBlue
+      top == prev.bottom
+      leading == super.leading
+      trailing == super.trailing
+
+    v.content.addSubview(visitorView)
+    com.visitProperties(visitor)
+    if not lasPropView.isNil:
+      lasPropView.makeLayout:
+        bottom == super.bottom
+
+    visitorView.onRemove do():
+      echo "hi"
+
+    inc idx
+
+
+
+  var lineView = new(View)
+  lineView.makeLayout:
+    top == prev.bottom
+    leading == super.leading
+    trailing == super.trailing
+    height == 2
+    backgroundColor: blackColor()
+  v.content.addSubview(lineView)
+
+  var bottomView = new(View)
+  bottomView.makeLayout:
+    top == prev.bottom
+    bottom == super
+    leading == super.leading
+    trailing == super.trailing
+
+    - Button as addComponent:
+      top == super
+      leading == super.leading
+      trailing == super.trailing
+      height == 16
+      height == super
+      title: "Add component"
+      onAction:
+        v.createComponentsView(n)
+
+  v.content.addSubview(bottomView)
 
 
 method onCompositionChanged*(v: EditorInspectorView, c: CompositionDocument) =
   procCall v.EditorTabView.onCompositionChanged(c)
   echo "EditorInspectorView onCompositionChanged"
+
+method setInspectedNode*(v: EditorInspectorView, n: Node) =
+  echo "EditorInspectorView selectedNode = ", (if n.isNil: "nil" else: n.name)
+  v.inspectedNodeChanged(n)
+
+method onEditorEvent*(v: EditorInspectorView, ev: EditorAPIEvent) =
+  if ev.kind == EditorMessageNodeSelectionChanged.toEditorMessageId:
+    echo "node selection changed!"
